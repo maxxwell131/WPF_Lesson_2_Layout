b64679bc2c9e2d1d89e5759a228b3659c3311277
maxxwell131 <maxxwell131@yahoo.com>
2017-12-24 14:04:36 +0200

    Добавьте файлы проекта.

---
+++ 07_IncCanvas/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+namespace _07_IncCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inkCanvas_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+            String gestures = "";
+
+            // Выборка "предпологаемых" гестур.
+            foreach (GestureRecognitionResult res in e.GetGestureRecognitionResults())
+                gestures += res.ApplicationGesture.ToString() + "; ";
+
+            // Отображаем название гестуры.
+            gestureName.Text = gestures;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Выборка всех режимов редактирования InkCanvas.
+            foreach (InkCanvasEditingMode mode in Enum.GetValues(typeof(InkCanvasEditingMode)))
+                lstEditingMode.Items.Add(mode);
+
+            lstEditingMode.SelectedItem = inkCanvas.EditingMode;
+        }
+    }
+}
